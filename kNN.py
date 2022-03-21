8fb184da103c9d92d8777cbac191f35dda9fbc99
Yuran <super-kangyuran@hotmail.com>
2022-03-21 11:45:58 +0800

    Update kNN.py

--- kNN.py
+++ kNN.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 import numpy as np 
 import operator
 
@@ -6,16 +8,12 @@ import operator
 
 Parameters:
     无
-
 Returns:
     groups - 数据集
     labels - 分类标签
-
 Modify:
     2022-03-13
-
 """
-
 def createDataSet():
     # 四组二维特征
     group = np.array([[1,101],[5,89],[108,5],[115,8]])
@@ -32,14 +30,11 @@ Parameters:
     dataSet - 用于训练的数据（训练集）
     labels - 分类标签
     k - kNN算法参数, 选择距离最小的k个点
-
 Returns:
     sortedClassCount[0][0] - 分类结果
-
 Modify:
     2022-03-13
 """
-
 def classify0(inX, dataSet, labels, k):
     # numpy函数shape[0]返回dataSet的行数
     dataSetSize = dataSet.shape[0]
@@ -69,6 +64,39 @@ def classify0(inX, dataSet, labels, k):
     # 返回次数最多的类别，即所要分类的类别
     return sortedClassCount[0][0]
 
+"""
+函数说明: 打开并解析文件，对数据进行分类：1代表不喜欢，2代表魅力一般，3代表极具魅力
+
+Parameters:
+    filename - 文件名
+Returns:
+    returnMat - 特征矩阵
+    classLabelVector - 分类Labe向量
+Modify:
+    2022-03-21
+"""
+def file2matrix(filename):
+    # 打开文件
+    fr = open(filename)
+    # 读取文件所有内容
+    arrayOLines = fr.readlines()
+    # 得到文件行数
+    numberOfLines = len(arrayOLines)
+    # 返回的NumPy矩阵，解析完成的数据：numberOfLines行，3列
+    returnMat = np.zeros((numberOfLines,3))
+    # 返回的分类标签向量
+    classLabelVector = []
+    # 行的索引值
+    index = 0
+    for line in arrayOLines:
+        # s.strip(rm)，当rm空时,默认删除空白符(包括'\n','\r','\t',' ')
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector        
+
 if __name__ == '__main__':
     # 创建数据集
     group, labels = createDataSet()
@@ -78,3 +106,4 @@ if __name__ == '__main__':
     test_class = classify0(test, group, labels, 3)
     # 打印分类结果
     print(test_class)
+    
